46fd840ab81086a2bea6ccf6df4fad5353c8cb58
TerryYan26 <103209100+TerryYan26@users.noreply.github.com>
2022-05-01 16:29:56 -0700

    update

--- skilldemo.java
+++ skilldemo.java
@@ -1,2 +1,5 @@
 public class skilldemo{
+    public static  int square(int n){
+        return n*n;
+    }
 }
---
+++ skilldemotest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class skilldemotest{
+    @Test
+    public void squaretest(){
+        assertEquals(3, skilldemo.square(3));
+    }
+}
